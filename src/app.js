34d401f5b1df5a3f3a13bc60cb77b0e44c26f023
Michael Vinci <michaeldvinci@gmail.com>
2016-04-07 11:10:16 -0400

    3 day quantification view as default

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
 var ajax = require('ajax');
-var items = [];
+var itemsTod = [];
+var itemsYes = [];
+var itemsTwo = [];
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
 var main = new UI.Card({
 	title: 'Pebble.js',
@@ -15,7 +18,7 @@ function timeConverter(UNIX_timestamp){
 	//var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 	//var year = a.getFullYear();
 	//var month = months[a.getMonth()];
-	var date = a.getDate();
+	//var date = a.getDate();
 	var hour = a.getHours();
 	var min = a.getMinutes();
 	var sec = a.getSeconds();
@@ -40,6 +43,7 @@ var today = new Date();
 var today1 = [today.getMonth()+1,today.getDate(),today.getFullYear()].join('/');
 var yesterday = [today.getMonth()+1,today.getDate()-1,today.getFullYear()].join('/');
 var twoDaysAgo = [today.getMonth()+1,today.getDate()-2,today.getFullYear()].join('/');
+var twoDays = days[ today.getDay() -2];
 
 var api_key='4a70eed4-e615-4f98-97d4-db2bc3b8691b';
 
@@ -56,20 +60,20 @@ ajax(
 
 		if (data.data.length == 0) {
 			console.log("No data for date "+twoDaysAgo);
-			items.push({
+			itemsTwo.push({
 				title: 'No time logged',
 				subtitle: 'Get to work!'
 			});
 		} else {
 			for (var i = 0; i<data.data.length; i++) {
-				items.push({ 
+				itemsTwo.push({ 
 					title: data.data[i].project, 
-					subtitle: timeConverter(data.data[i].time) + ' for '+toHHMMSS(data.data[i].duration.toString())
+					subtitle: 'Time spent: '+toHHMMSS(data.data[i].duration.toString())
 				});
 			}
 		}
 
-		console.log("items", items);
+		console.log("items", itemsTwo);
 
 	},
 	function(error) {
@@ -91,20 +95,20 @@ ajax(
 
 		if (data.data.length == 0) {
 			console.log("No data for date "+yesterday);
-			items.push({
+			itemsYes.push({
 				title: 'No time logged',
 				subtitle: 'Get to work!'
 			});
 		} else {
 			for (var i = 0; i<data.data.length; i++) {
-				items.push({ 
+				itemsYes.push({ 
 					title: data.data[i].project, 
-					subtitle: timeConverter(data.data[i].time) + ' for '+toHHMMSS(data.data[i].duration.toString())
+					subtitle: 'Time spent: '+toHHMMSS(data.data[i].duration.toString())
 				});
 			}
 		}
 
-		console.log("items", items);
+		console.log("items", itemsYes);
 
 	},
 	function(error) {
@@ -126,25 +130,31 @@ ajax(
 
 		if (data.data.length == 0) {
 			console.log("No data for date "+today1);
-			items.push({
+			itemsTod.push({
 				title: 'No time logged',
 				subtitle: 'Get to work!'
 			});
 		} else {
 			for (var i = 0; i<data.data.length; i++) {
-				items.push({ 
+				itemsTod.push({ 
 					title: data.data[i].project, 
-					subtitle: timeConverter(data.data[i].time) + ' for '+toHHMMSS(data.data[i].duration.toString())
+               subtitle: 'Time spent: '+toHHMMSS(data.data[i].duration.toString())
 				});
 			}
 		}
 
-		console.log("items", items);
+		console.log("items", itemsTod);
 
 		var dailyProjects = new UI.Menu({
 			sections: [{
-				title: 'WakaTime',
-				items: items
+				title: twoDays,
+				items: itemsTwo
+			}, {
+				title: 'Yesterday',
+				items: itemsYes
+			}, {
+				title: 'Today',
+            items: itemsTod
 			}]
 		});
 
@@ -155,47 +165,3 @@ ajax(
 		console.log('Failed fetching WakaTime data: ' + error);
 	}
 );
-
-main.on('click', 'up', function(e) {
-	var menu = new UI.Menu({
-		sections: [{
-			items: [{
-				title: 'Pebble.js',
-				icon: 'images/menu_icon.png',
-				subtitle: 'Can do Menus'
-			},{
-				title: 'Second Item',
-				subtitle: 'Subtitle Text'
-			}]
-		}]
-	});
-	menu.on('select', function(e) {
-		console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-		console.log('The item is titled "' + e.item.title + '"');
-	});
-	menu.show();
-});
-
-
-/**
-main.on('click', 'select', function(e) {
-	var wind = new UI.Window();
-	var textfield = new UI.Text({
-		position: new Vector2(0, 50),
-		size: new Vector2(144, 30),
-		font: 'gothic-24-bold',
-		text: 'Text Anywhere!',
-		textAlign: 'center'
-	});
-	wind.add(textfield);
-	wind.show();
-});
-
-main.on('click', 'down', function(e) {
-	var card = new UI.Card();
-	card.title('A Card');
-	card.subtitle('Is a Window');
-	card.body('The simplest window type in Pebble.js.');
-	card.show();
-});
-**/
